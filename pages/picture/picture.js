20f4d20626b65df5a2400db92a77e6b1338fc105
janjan <514737546@qq.com>
2019-03-26 13:16:53 +0800

    选区域截图显示

--- pages/picture/picture.js
+++ pages/picture/picture.js
@@ -2,11 +2,17 @@ const util = require('../../utils/util.js')
 const app = getApp()
 
 Page({
-    data: {},
+    data: {
+        sceneList: [],
+    },
     onLoad: function () {
         const screenWidth = app.jonGetSysinfo('screenWidth')
             , screenHeight = app.jonGetSysinfo('screenHeight')
+            , pixelRatio = app.jonGetSysinfo('pixelRatio') || 2
             , picturePath = app.jonGetPicture();
+        console.log(screenWidth, screenHeight, pixelRatio);
+        this.pixelRatio = pixelRatio;
+
         this.ctxSelect = wx.createCanvasContext('J_canvasSelect');
         this.ctxSelect.strokeStyle = "red";
         this.ctxSelect.fillStyle = "white";
@@ -55,21 +61,51 @@ Page({
     },
     jonTouchend(e) {
         console.log(this.screenPos);
-        const width = this.screenPos.maxX - this.screenPos.minX;
-        const height = this.screenPos.maxY - this.screenPos.minY;
-        this.ctxSelect.restore();
-        this.ctxSelect.globalAlpha = .5;
+        const that = this
+            , width = this.screenPos.maxX - this.screenPos.minX
+            , height = this.screenPos.maxY - this.screenPos.minY;
+        wx.canvasToTempFilePath({
+            x: that.screenPos.minX,
+            y: that.screenPos.minY,
+            width: width, height: height,
+            destWidth: width, destHeight: height,
+            canvasId: 'J_canvasBg',
+            success(res) {
+                console.log(res.tempFilePath);
+                const sceneList = that.data.sceneList
+                    , id = sceneList.length + 1;
+                sceneList.push({
+                    id: id,
+                    path: res.tempFilePath,
+                    x: that.screenPos.minX * that.pixelRatio,
+                    y: that.screenPos.minY * that.pixelRatio,
+                    width: width * that.pixelRatio,
+                    height: height * that.pixelRatio,
+                    zIndex: 900 + id
+                });
+                that.setData({
+                    sceneList: sceneList
+                });
+            }
+        });
+
+        // this.ctxSelect.restore();
+        // // 全局透明度
+        // this.ctxSelect.globalAlpha = .5;
+        // // 阴影
         // this.ctxSelect.shadowOffsetX = 5;
         // this.ctxSelect.shadowOffsetY = 5;
         // this.ctxSelect.shadowBlur = 5;
         // this.ctxSelect.shadowColor = 'rgba(0, 0, 0, 199)';
-        this.ctxSelect.fillRect(this.screenPos.minX, this.screenPos.minY, width, height);
-        this.ctxSelect.moveTo(this.screenPos.minX, this.screenPos.minY);
-        this.ctxSelect.lineTo(this.screenPos.minX, this.screenPos.maxY);
-        this.ctxSelect.lineTo(this.screenPos.maxX, this.screenPos.maxY);
-        this.ctxSelect.lineTo(this.screenPos.maxX, this.screenPos.minY);
-        this.ctxSelect.lineTo(this.screenPos.minX, this.screenPos.minY);
-        this.ctxSelect.stroke();
-        this.ctxSelect.draw();
+        // // 画矩形
+        // this.ctxSelect.fillRect(this.screenPos.minX, this.screenPos.minY, width, height);
+        // // 画边框线
+        // this.ctxSelect.moveTo(this.screenPos.minX, this.screenPos.minY);
+        // this.ctxSelect.lineTo(this.screenPos.minX, this.screenPos.maxY);
+        // this.ctxSelect.lineTo(this.screenPos.maxX, this.screenPos.maxY);
+        // this.ctxSelect.lineTo(this.screenPos.maxX, this.screenPos.minY);
+        // this.ctxSelect.lineTo(this.screenPos.minX, this.screenPos.minY);
+        // this.ctxSelect.stroke();
+        // this.ctxSelect.draw();
     },
 })
